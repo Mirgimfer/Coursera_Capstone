15499dd4d116d0658530422303d58db919291365
Mirgimfer <74135493+Mirgimfer@users.noreply.github.com>
2021-04-24 14:49:42 +0200

    Add files via upload

---
+++ Peer-graded Assignment.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>Peer-graded Assignment: Capstone Project Notebook<h1>
+
+# <i> This notebook will be mainly used for the capstone project.</i>
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
